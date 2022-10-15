eef3c79df141044f0d0c571778a2a3e5d373384e
Akshit Anchan <hello@akshitanchan.com>
2022-10-15 11:04:26 +0530

    Update pdf-compressor.py

--- assets/pdf-compressor.py
+++ assets/pdf-compressor.py
@@ -10,15 +10,15 @@ def compress(input_file_path, output_file_path, power=0):
     }
 
     if not os.path.isfile(input_file_path):
-        print("Error: invalid path for input PDF file")
+        print("Error: Invalid path for input PDF file")
         sys.exit(1)
 
     if input_file_path.split('.')[-1].lower() != 'pdf':
-        print("Error: input file is not a PDF")
+        print("Error: Input file is not a PDF")
         sys.exit(1)
 
     gs = get_ghostscript_path()
-    print("Compress PDF...")
+    print("Compressing the PDF...")
     initial_size = os.path.getsize(input_file_path)
     subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
                     '-dPDFSETTINGS={}'.format(quality[power]),
@@ -30,7 +30,7 @@ def compress(input_file_path, output_file_path, power=0):
     ratio = 1 - (final_size / initial_size)
     print("Compression by {0:.0%}.".format(ratio))
     print("Final file size is {0:.1f}MB".format(final_size / 1000000))
-    print("Done.")
+    print("Done. File has been compressed.")
 
 
 def get_ghostscript_path():
@@ -46,12 +46,12 @@ def main():
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument('input', help='Relative or absolute path of the input PDF file')
-    parser.add_argument('-o', '--out', help='Relative or absolute path of the output PDF file')
-    parser.add_argument('-c', '--compress', type=int, help='Compression level from 0 to 4')
-    parser.add_argument('-b', '--backup', action='store_true', help="Backup the old PDF file")
+    parser.add_argument('input', help='Relative or absolute path of the input PDF file.')
+    parser.add_argument('-o', '--out', help='Relative or absolute path of the output PDF file.')
+    parser.add_argument('-c', '--compress', type=int, help='Compression level from 0 to 4.')
+    parser.add_argument('-b', '--backup', action='store_true', help="Backup the old PDF file.")
     parser.add_argument('--open', action='store_true', default=False,
-                        help='Open PDF after compression')
+                        help='Open PDF after compression.')
     args = parser.parse_args()
 
     if not args.compress:
